dfeff291195cba4e5c3f8f409c7d9a5358fb357f
HOANGDUY <nguyenhoangduy12dmt01@gmail.com>
2021-01-25 16:04:25 +0700

    Message

---
+++ webpack.mix.js
@@ -0,0 +1,32 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+ 
+mix.pug = require('laravel-mix-pug');
+mix
+    .pug('resources/views/_*.pug', '../../public/html/', {
+        pug: {
+            pretty: true,
+            debug: true
+        }
+    })
+    .sass('resources/sass/app.scss', 'public/css')
+    .sass('resources/sass/main.scss', 'public/css')
+    .sass('resources/sass/index.scss', 'public/css');
+
+    // .sass('resources/sass/right.scss', 'public/css');
+
+// mix.js('resources/js/app.js', 'public/js')
+
+// .disableNotifications();
+
